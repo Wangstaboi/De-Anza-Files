1e9672971f54f4701c77ba978a05f08e718a12ad
Ryan Wang <bubmom9@gmail.com>
2023-01-24 11:55:13 -0800

    Added boilerplate for StudentEsa.cpp

---
+++ CIS22C/HM1/StudentEsa.cpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <iostream>
+#include "Student.h"
+
+using namespace std;
+
+class StudentEsa
+{
+private:
+    Student* *sap;
+    Student* *tp;
+    int cnum;
+    int cmz;
+
+public:
+    //Default Constructor
+    StudentEsa() {
+
+
+
+    }
+
+    StudentEsa(StudentEsa&) {
+
+
+    }
+
+    StudentEsa(int cmz) {
+
+
+    }
+
+    ~StudentEsa() {
+
+
+    }
+
+    int getNum() {
+
+    }
+
+    Student* get(int) {
+
+
+    }
+
+    int set(Student*, int) {
+
+
+    }
+
+    int insert(Student*, int) {
+
+
+    }
+
+    int remove(int) {
+
+
+    }
+
+    int append(Student*) {
+
+
+
+    }
+    
+    int prepend(Student*) {
+
+        
+    }
+};
+
+
+
+
