c6d7179606cf265ca76aa201a11b88413c24bfc4
Wangstaboi <bubmom9@gmail.com>
2023-02-08 01:19:22 -0800

    Finished lab4, adding module5

--- CIS21JA/module_4/lab4.asm
+++ CIS21JA/module_4/lab4.asm
@@ -11,6 +11,9 @@ INCLUDE Irvine32.inc
 promptHr BYTE "Enter hour of get up time: ",0
 promptMin BYTE "Enter minute of get up time: ",0
 promptSnooze BYTE "Enter number of minutes of snooze: ",0
+string1 BYTE "Alarm set for ",0
+string2 BYTE " Hour and ",0
+string3 BYTE " minute",0
 
 .code
 main PROC
@@ -19,30 +22,57 @@ mov edx, OFFSET promptHr	; moves promptHR into edx
 call writeString		; Writes out "Enter hour of get up time"
 call readDec			; Takes user input for hour of get up time, stores in eax
 
+mov bl, al				; Moves hour to bl
+
 mov edx, OFFSET promptMin	; moves promptMin into edx
-call writeString		; Writes out "Enter minute of get up time"
-mov ecx, eax			; Stores hour in edx
-call readDec			; stores minutes in eax
-mov ebx, 0			; stores minutes in ebx
-mov ebx, eax
+call writeString		; Writes out promptMin to console
+call readDec			; Takes user input (should be for min), stores in eax
+
+mov cl, al				; Moves minutes to cl
 
 mov edx, OFFSET promptSnooze	; moves promptSnooze into edx
-call writeString		; Writes out "Enter num of min of snooze"
-call readDec			; Stores min of snooze into eax
+call writeString		; Writes out promptSnooze to console
+call readDec			; Takes user input (for snooze time in min) stores in eax
 
-; hr: ecx, min: ebx, snoozeMIn: eax
+mov dl, al
 
-; swap ecx and eax
-xchg ecx, eax
+; hour = bl, min = cl, snooze = dl
+; Checkpoint - Everything until here is correct
+
+mov al, bl				; Move hour from bl to al
+mov bl, 60				; Move number 60 to bl
+mul bl					; Multiply al (hour) by 60 to convert to minutes
+movzx cx, cl			; Extend cl to cx
+movzx dx, dl			; Extend dl to dx
+add ax, cx				; Add minutes with hours
+sub ax, dx				; Subtract snooze time from total
 
 
-; Convert Hours to Min
-mul 60 ; result in edx
+div bl					; Divide ax by 60, AL = hours, AH = minutes
 
-add edx, ebx		; Add min to hours (in minute form)
-sub edx, ecx		; Subtracting snooze time from total
+; Checkpoint
+; AL: 10 AH: 48 - Done
+mov edx, OFFSET string1 ; Moving string1 into edx
+call writeString		; Write string1 to console
 
-; Convert back to hrs and min
+mov bl, al				; Move al to bl
+mov cl, ah				; Move ah to cl
+
+movzx eax, bl			; Extend al to edx
+call writeDec			; Write hours to console
+
+mov edx, OFFSET string2 ; Move string2 to edx
+call writeString		; write string2 to console
+
+movzx eax, cl			; Extend ah to edx
+call writeDec			; Write minutes to console
+
+mov edx, OFFSET string3 ; Move string3 to edx
+call writeString		; Write string3 to console
+
+
+;call dumpRegs
+
 
 
 
@@ -74,12 +104,12 @@ b. explain why CF and OF flags have that value
 mov al, 0C0h 
 
 add al, 50h     
-; a. ZF =   SF =   CF =   OF =
-; b. explanation for CF:
-;    explanation for OF:
+; a. ZF = 1  SF = 0  CF = 1  OF = 1
+; b. explanation for CF: Add operation too big for al, therefore carry flag is 1 	
+;    explanation for OF: MSB is set to 1, therefore OF is 1
 
 sub al, 0A0h     
-; a. ZF =   SF =   CF =   OF =
-; b. explanation for CF:
-;    explanation for OF:
+; a. ZF = 1  SF = 0  CF = 0  OF = 0
+; b. explanation for CF: Subtracting means smaller number, means no way itll have to carry (ends in 0 so positive)
+;    explanation for OF: Subtracting means smaller, so no overflow (also its positive)
 @
