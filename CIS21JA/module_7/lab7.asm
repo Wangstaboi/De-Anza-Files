5cee420d0d7461ae13f87911f4a378d366447298
Wangstaboi <bubmom9@gmail.com>
2023-03-06 14:45:59 -0800

    Added Prompt and User Input w/ Stack - Lab 7

--- CIS21JA/module_7/lab7.asm
+++ CIS21JA/module_7/lab7.asm
@@ -1,6 +1,25 @@
 INCLUDE Irvine32.inc
 
 ; Part 1 
+
+zeroFill MACRO			; Adds a zero if value is less than 10
+	cmp eax, 10
+	jg addZero
+	call writeDec
+	jmp end				; May change later
+
+	addZero:
+		push eax		; save eax into stack
+		mov eax, 0		; Moves zero into eax
+		call writeDec
+		pop eax
+		call writeDec
+
+	end:
+		call crlf
+		
+ENDM
+
 .data
 promptHr BYTE "Enter hour of get up time: ",0
 promptMin BYTE "Enter minute of get up time: ",0
@@ -10,7 +29,7 @@ string2 BYTE " Hour and ",0
 string3 BYTE " minute",0
 colon BYTE ":",0
 errorMsg BYTE "Invalid Input, Try Again.",0
-timeArr BYTE, 2 DUP(?)
+timeArr BYTE 2 DUP(?)
 
 
 ; STACK IS LIFO
@@ -20,28 +39,29 @@ timeArr BYTE, 2 DUP(?)
 .code
 main PROC
 
-
 ; Lab 7 Version
 hour:
 	sub esp, 4					; saves room for return value (return value -- esp)
-	push OFFSET promptHr
+	push OFFSET promptHr 
 	push OFFSET errorMsg
 	push 23
 	call readTime
+	pop eax						; 
 
 min:
 	sub esp, 4					; saves room for return value (return value -- esp)
-	push OFFSET promptHr
+	push OFFSET promptMin
 	push OFFSET errorMsg
 	push 59
 	call readTime
-
+	pop eax
 snooze:
 	sub esp, 4					; saves room for return value (return value -- esp)
 	push OFFSET promptSnooze
 	push OFFSET errorMsg
 	push 59
 	call readTime
+	pop eax
 
 main ENDP
 
@@ -49,7 +69,7 @@ main ENDP
 ;	- push []
 
 
-PROC readTime
+readTime PROC
 	basePointer:
 		push ebp		; save current ebp
 		mov ebp, esp	; save esp into ebp now so we can do shit with it if we want i guess
@@ -64,12 +84,9 @@ PROC readTime
 		jg error			; If error, jump to error block
 
 		; if not error do following:
-		mov [ebp + 20], eax	; Moves user input (eax) into [ebp + 20] (top of stack)
+		mov [ebp + 16], eax	; Moves user input (eax) into [ebp + 20] (top of stack)
 		pop ebp				; pops out bottom (ebp) back into ebp
 		ret 16				; pops out eip and next 3 stack elements (16 bytes)
-
-
-		;push eax			; Moves current eax value (SHOULD be current hr)
 
 	error:
 		mov edx, OFFSET errorMsg	; move error to edx
@@ -78,7 +95,13 @@ PROC readTime
 		jmp writeFunc
 
 readTime ENDP
+	
+calcTime PROC
 
+
+calcTime ENDP
+
+	exit
 END main
 
 
