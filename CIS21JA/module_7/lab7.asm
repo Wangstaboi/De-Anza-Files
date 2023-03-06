0566b000e6a75a0f834091781a8ea011ae9a1c16
Wangstaboi <bubmom9@gmail.com>
2023-03-05 23:41:41 -0800

    Update Lab 7

--- CIS21JA/module_7/lab7.asm
+++ CIS21JA/module_7/lab7.asm
@@ -20,186 +20,67 @@ timeArr BYTE, 2 DUP(?)
 .code
 main PROC
 
-; Lab 5 Version
-hour:
-	mov edx, OFFSET promptHr	; moves promptHR into edx
-	
-	call writeString		; Writes out "Enter hour of get up time"
-	call readDec			; Takes user input for hour of get up time, stores in eax
-	;call CrLf
-	mov ecx, 23				; cl equals 23
-	cmp eax, ecx
-	ja errorHr
 
 ; Lab 7 Version
 hour:
+	sub esp, 4					; saves room for return value (return value -- esp)
+	push OFFSET promptHr
+	push OFFSET errorMsg
+	push 23
+	call readTime
+
+min:
+	sub esp, 4					; saves room for return value (return value -- esp)
 	push OFFSET promptHr
 	push OFFSET errorMsg
 	push 59
 	call readTime
 
-
-mov bl, al				; Moves hour to bl
-
-min:
-	mov edx, OFFSET promptMin	; moves promptMin into edx
-	
-	call writeString		; Writes out promptMin to console
-	call readDec			; Takes user input (should be for min), stores in eax
-	;call CrLf
-	mov ecx, 59
-	cmp eax, ecx
-	ja errorMin
-
-mov bh, al				; Moves minutes to cl
-
 snooze:
-	mov edx, OFFSET promptSnooze	; moves promptSnooze into edx
-	
-	call writeString		; Writes out promptSnooze to console
-	call readDec			; Takes user input (for snooze time in min) stores in eax
-	;call CrLf
-	mov ecx, 59
-	cmp eax, ecx
-	ja errorSnooze
+	sub esp, 4					; saves room for return value (return value -- esp)
+	push OFFSET promptSnooze
+	push OFFSET errorMsg
+	push 59
+	call readTime
 
-	mov dl, al
-
-; hour = bl, min = bh, snooze = dl
-; Checkpoint - Everything until here is correct
-
-mov al, bl				; Move hour from bl to al
-mov bl, 60				; Move number 60 to bl
-mul bl					; Multiply al (hour) by 60 to convert to minutes
-movzx cx, bh			; Extend cl to cx
-movzx dx, dl			; Extend dl to dx
-add ax, cx				; Add minutes with hours
-sub ax, dx				; Subtract snooze time from total
-js errorSub
-
-div bl					; Divide ax by 60, AL = hours, AH = minutes
-mov bl, al
-mov bh, ah
-
-; Checkpoint
-; AL: 10 AH: 48 - Done
-; BL: 10 BH: 48
-
-writeHr:
-	mov cl, 10
-	cmp bl, cl
-	jb formatHr
-	mov edx, OFFSET string1
-	call writeString
-	movzx eax, bl
-	call writeDec
-
-	mov edx, OFFSET colon
-	call writeString
-	
-writeMin:
-	mov cl, 10
-	cmp bh, cl
-	jb formatMin
-	;mov edx, OFFSET string1
-	;call writeString
-	movzx eax, bh
-	call writeDec
-	call CrLf
-	
-	jmp hour
-
-
-
-;mov edx, OFFSET string1 ; Moving string1 into edx
-;call writeString		; Write string1 to console
-
-;mov bl, al				; Move al to bl
-;mov cl, ah				; Move ah to cl
-
-;movzx eax, bl			; Extend al to edx
-;call writeDec			; Write hours to console
-
-;mov edx, OFFSET string2 ; Move string2 to edx
-;call writeString		; write string2 to console
-
-;movzx eax, bh			; Extend ah to edx
-;call writeDec			; Write minutes to console
-
-;mov edx, OFFSET string3 ; Move string3 to edx
-;call writeString		; Write string3 to console
-
-;jmp thing
-
-;call dumpRegs
-
-errorHr:
-	mov edx, OFFSET errorMsg	; move error to edx
-	call writeString
-	call CrLf
-	jmp hour
-
-errorMin:
-	mov edx, OFFSET errorMsg	; move error to edx
-	call writeString
-	call CrLf
-	jmp min
-
-errorSnooze:
-	mov edx, OFFSET errorMsg	; move error to edx
-	call writeString
-	call CrLf
-	jmp snooze
-
-errorSub:
-	mov edx, OFFSET errorMsg	; move error to edx
-	call writeString
-	call CrLf
-	jmp thing
-
-formatHr:
-	mov edx, OFFSET string1
-	call writeString
-	mov eax, 0
-	call writeDec
-	movzx eax, bl
-	call writeDec
-	mov edx, OFFSET colon
-	call writeString
-	jmp writeMin
-
-formatMin:
-	mov eax, 0
-	call writeDec
-	movzx eax, bh
-	call writeDec
-	jmp hour
-
-thing:
-	exit
 main ENDP
-
-
-END main
 
 ; Stack stuff:
 ;	- push []
 
 
 PROC readTime
-	push ebp		; save current ebp
-	mov ebp, esp	; save esp into ebp now so we can do shit with it if we want i guess
-	push edx		; save whatever was already in edx (might be unnecessary)
+	basePointer:
+		push ebp		; save current ebp
+		mov ebp, esp	; save esp into ebp now so we can do shit with it if we want i guess
+		;push edx		; save whatever was already in edx (might be unnecessary)
 
-	mov edx, [ebp + 20] ; Moves [ebp + 20] into edx (SHOULD be promptHr)
-	call writeString
+	writeFunc:
+		mov edx, [ebp + 16] ; Moves [ebp + 20] into edx (SHOULD be promptHr)
+		call writeString
+		call readInt
+		;push eax			; Push user input into stack
+		cmp eax, [ebp + 8]  ; compares eax (user inputted hour) with [ebp + 12] (SHOULD be max hr)
+		jg error			; If error, jump to error block
 
-	call readDec
-	cmp eax, [ebp + 12] ; compares eax (user inputted hour) with [ebp + 12] (SHOULD be max hr)
-	ja errorHr
-	push eax			; Moves current eax value (SHOULD be current hr)
+		; if not error do following:
+		mov [ebp + 20], eax	; Moves user input (eax) into [ebp + 20] (top of stack)
+		pop ebp				; pops out bottom (ebp) back into ebp
+		ret 16				; pops out eip and next 3 stack elements (16 bytes)
+
+
+		;push eax			; Moves current eax value (SHOULD be current hr)
+
+	error:
+		mov edx, OFFSET errorMsg	; move error to edx
+		call writeString
+		call CrLf
+		jmp writeFunc
 
 readTime ENDP
+
+END main
+
 
 
 
